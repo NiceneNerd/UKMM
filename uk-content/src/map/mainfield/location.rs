5feb8c2058aea8f981904a63a8d00380b0734eb0
Caleb Smith <caleb@myrvmail.com>
2022-05-26 16:15:10 -0500

    Complete Location

--- uk-content/src/map/mainfield/location.rs
+++ uk-content/src/map/mainfield/location.rs
@@ -1,3 +1,5 @@
+use std::collections::BTreeSet;
+
 use crate::{
     prelude::Mergeable,
     util::{DeleteVec, SortedDeleteMap},
@@ -82,5 +84,97 @@ impl From<Location> for Byml {
                     .collect()
             })
             .collect()
+    }
+}
+
+impl Mergeable<Byml> for Location {
+    fn diff(&self, other: &Self) -> Self {
+        Self(
+            other
+                .0
+                .iter()
+                .filter_map(|(message, other_entries)| {
+                    if let Some(self_entries) = self.0.get(message) {
+                        if self_entries != other_entries {
+                            Some((message.clone(), self_entries.diff(other_entries), false))
+                        } else {
+                            None
+                        }
+                    } else {
+                        Some((message.clone(), other_entries.clone(), false))
+                    }
+                })
+                .chain(self.0.keys().filter_map(|message| {
+                    (!other.0.contains_key(message))
+                        .then(|| (message.clone(), Default::default(), true))
+                }))
+                .collect(),
+        )
+    }
+
+    fn merge(&self, diff: &Self) -> Self {
+        let messages: BTreeSet<&String> = self.0.keys().chain(diff.0.keys()).collect();
+        Self(messages.into_iter().map(|message| {
+            let (self_entries, diff_entries) = (self.0.get(message), diff.0.get(message));
+            if let Some(self_entries) = self_entries && let Some(diff_entries) = diff_entries {
+                (message.clone(), self_entries.merge(diff_entries), diff.0.is_delete(message).unwrap_or_default())
+            } else {
+                (message.clone(), diff_entries.or(self_entries).cloned().unwrap(), diff.0.is_delete(message).unwrap_or_default())
+            }
+        }).collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+    use roead::byml::Byml;
+
+    fn load_location() -> Byml {
+        Byml::from_binary(
+            &roead::yaz0::decompress(&std::fs::read("test/Map/MainField/Location.smubin").unwrap())
+                .unwrap(),
+        )
+        .unwrap()
+    }
+
+    fn load_mod_location() -> Byml {
+        Byml::from_binary(
+            &roead::yaz0::decompress(
+                &std::fs::read("test/Map/MainField/Location.mod.smubin").unwrap(),
+            )
+            .unwrap(),
+        )
+        .unwrap()
+    }
+
+    #[test]
+    fn serde() {
+        let byml = load_location();
+        let location = super::Location::try_from(&byml).unwrap();
+        let data = Byml::from(location.clone()).to_binary(roead::Endian::Big);
+        let byml2 = Byml::from_binary(&data).unwrap();
+        let location2 = super::Location::try_from(&byml2).unwrap();
+        assert_eq!(location, location2);
+    }
+
+    #[test]
+    fn diff() {
+        let byml = load_location();
+        let location = super::Location::try_from(&byml).unwrap();
+        let byml2 = load_mod_location();
+        let location2 = super::Location::try_from(&byml2).unwrap();
+        let _diff = location.diff(&location2);
+    }
+
+    #[test]
+    fn merge() {
+        let byml = load_location();
+        let location = super::Location::try_from(&byml).unwrap();
+        let byml2 = load_mod_location();
+        let location2 = super::Location::try_from(&byml2).unwrap();
+        let diff = location.diff(&location2);
+        let merged = location.merge(&diff);
+        assert_eq!(merged, location2);
     }
 }
