00e0e6545c5e3627513ab72fb37e6e970ea0de0c
Caleb Smith <caleb@myrvmail.com>
2022-06-08 15:06:58 -0500

    Add DropTable actor info

--- uk-content/src/actor/params/drop.rs
+++ uk-content/src/actor/params/drop.rs
@@ -1,6 +1,9 @@
-use crate::{prelude::*, Result, UKError};
+use crate::{actor::InfoSource, prelude::*, Result, UKError};
 use indexmap::IndexMap;
-use roead::aamp::*;
+use roead::{
+    aamp::*,
+    byml::{Byml, Hash},
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, Clone, PartialEq, Deserialize, Serialize)]
@@ -97,9 +100,39 @@ impl Mergeable<ParameterIO> for DropTable {
     }
 }
 
+impl InfoSource for DropTable {
+    fn update_info(&self, info: &mut roead::byml::Hash) -> crate::Result<()> {
+        info.insert(
+            "drops".to_owned(),
+            self.0
+                .iter()
+                .map(|(name, table)| -> Result<(String, Byml)> {
+                    Ok((name.clone(), {
+                        let count = table
+                            .param("ColumnNum")
+                            .ok_or(UKError::MissingAampKey("Drop table missing column count"))?
+                            .as_int()?;
+                        (1..=count)
+                            .map(|i| -> Result<Byml> {
+                                Ok(table
+                                    .param(&format!("ItemName{:02}", i))
+                                    .ok_or(UKError::MissingAampKey("Drop table missing item name"))?
+                                    .as_string()?
+                                    .to_owned()
+                                    .into())
+                            })
+                            .collect::<Result<_>>()
+                    }?))
+                })
+                .collect::<Result<_>>()?,
+        );
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::prelude::*;
+    use crate::{actor::InfoSource, prelude::*};
 
     #[test]
     fn serde() {
@@ -136,7 +169,6 @@ mod tests {
         .unwrap();
         let drop2 = super::DropTable::try_from(&pio2).unwrap();
         let diff = drop.diff(&drop2);
-        println!("{}", serde_json::to_string_pretty(&diff).unwrap());
     }
 
     #[test]
@@ -160,5 +192,33 @@ mod tests {
         let diff = drop.diff(&drop2);
         let merged = drop.merge(&diff);
         assert_eq!(drop2, merged);
+    }
+
+    #[test]
+    fn info() {
+        let actor = crate::tests::test_base_actorpack("Enemy_Guardian_A");
+        let pio = roead::aamp::ParameterIO::from_binary(
+            actor
+                .get_file_data("Actor/DropTable/Enemy_Guardian_A.bdrop")
+                .unwrap(),
+        )
+        .unwrap();
+        let drop = super::DropTable::try_from(&pio).unwrap();
+        let mut info = roead::byml::Hash::new();
+        drop.update_info(&mut info).unwrap();
+        assert_eq!(
+            info["drops"].as_hash().unwrap()["Normal"]
+                .as_array()
+                .unwrap(),
+            vec![
+                roead::byml::Byml::String("Item_Enemy_27".to_owned()),
+                roead::byml::Byml::String("Item_Enemy_28".to_owned()),
+                roead::byml::Byml::String("Item_Enemy_26".to_owned()),
+                roead::byml::Byml::String("Item_Enemy_29".to_owned()),
+                roead::byml::Byml::String("Item_Enemy_30".to_owned()),
+                roead::byml::Byml::String("Item_Enemy_31".to_owned()),
+            ]
+            .as_slice()
+        );
     }
 }
