f5d65fe90c4de2182c88557a4b11b9f741b0c4b2
Caleb Smith <c.smith@tuta.io>
2022-09-20 09:24:34 -0500

    Selectable log

--- assets/components/Log/Log.js
+++ assets/components/Log/Log.js
@@ -1,48 +1,29 @@
 export class Log extends Element {
-    constructor(props) {
-        super(props);
-        this.levelColor = this.levelColor.bind(this);
-    }
-
     this(props) {
         this.props = props;
     }
 
-    levelColor(level) {
-        switch (level) {
-            case "WARN":
-                return "gold";
-            case "INFO":
-                return "forestgreen";
-            case "DEBUG":
-                return "royalblue";
-            case "ERROR":
-                return "red";
-            default:
-                return "color(panel-text)";
-        }
-    }
-
     componentDidMount() {
-        this.vlist = document.querySelector("#Log").vlist;
+        this.list = document.querySelector("#Log");
     }
 
     componentDidUpdate() {
-        this.vlist.advanceTo(this.props.logs.length - 1);
+        // this.vlist.advanceTo(this.props.logs.length - 1);
+        this.list.scrollTop = this.list.scrollHeight;
     }
 
     render() {
         this.post(this.componentDidUpdate);
         return (
-            <div #Log styleset={__DIR__ + "Log.css#Log"} selectable>
+            <plaintext #Log styleset={__DIR__ + "Log.css#Log"} readonly>
                 {this.props.logs.map(entry => (
-                    <div key={entry.msg}>
+                    <text key={entry.msg}>
                         [<span class="time">{entry.timestamp}</span>{" "}
-                        <span class="level" style={`color: ${this.levelColor(entry.level)}`}>{entry.level}</span>]{" "}
+                        <span class={"level " + entry.level}>{entry.level}</span>]{" "}
                         <span class="msg">{entry.args}</span>
-                    </div>
+                    </text>
                 ))}
-            </div>
+            </plaintext>
         );
     }
 }
