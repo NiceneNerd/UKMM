4c585dabb565216c95ad46931bcf6633c699dc48
Caleb Smith <c.smith@tuta.io>
2022-09-21 17:00:33 -0500

    Work on stupid path to tree thing

--- src/gui/info.rs
+++ src/gui/info.rs
@@ -1,4 +1,5 @@
 use std::{
+    cell::RefCell,
     collections::BTreeSet,
     path::{Path, PathBuf},
 };
@@ -35,19 +36,102 @@ pub fn render_mod_info(mod_: &Mod, ui: &mut Ui) {
     });
 }
 
-#[derive(PartialEq, Eq, Hash, PartialOrd, Default)]
+// A type to represent a path, split into its component parts
+#[derive(Debug)]
 struct PathNode {
-    name: String,
-    path: PathBuf,
-    children: BTreeSet<PathNode>,
+    parts: Vec<String>,
+}
+impl PathNode {
+    pub fn new(path: &str) -> Self {
+        Self {
+            parts: path.to_string().split('/').map(|s| s.into()).collect(),
+        }
+    }
+
+    pub fn path(&self) -> PathBuf {
+        self.parts.iter().map(|p| p.as_str()).collect()
+    }
 }
 
-impl From<&BTreeSet<String>> for PathNode {
-    fn from(files: &BTreeSet<String>) -> Self {
-        files.iter().fold(PathNode::default(), |root, file| root)
+// A recursive type to represent a directory tree.
+// Simplification: If it has children, it is considered
+// a directory, else considered a file.
+#[derive(Debug, Clone)]
+struct Dir {
+    name: String,
+    children: Vec<Dir>,
+}
+
+impl Dir {
+    fn new(name: &str) -> Dir {
+        Dir {
+            name: name.into(),
+            children: Vec::<Dir>::new(),
+        }
+    }
+
+    fn find_child(&mut self, name: &str) -> Option<&mut Dir> {
+        self.children.iter_mut().find(|c| c.name == name)
+    }
+
+    fn add_child<T>(&mut self, leaf: T) -> &mut Self
+    where
+        T: Into<Dir>,
+    {
+        self.children.push(leaf.into());
+        self
+    }
+}
+
+fn dir(val: &str) -> Dir {
+    Dir::new(val)
+}
+
+fn build_tree(node: &mut Dir, parts: &Vec<String>, depth: usize) {
+    if depth < parts.len() {
+        let item = &parts[depth];
+
+        let mut dir = match node.find_child(&item) {
+            Some(d) => d,
+            None => {
+                let d = Dir::new(&item);
+                node.add_child(d);
+                match node.find_child(&item) {
+                    Some(d2) => d2,
+                    None => unreachable!(),
+                }
+            }
+        };
+        build_tree(dir, parts, depth + 1);
+    }
+}
+
+fn render_dir(dir: &Dir, ui: &mut Ui) {
+    if !dir.children.is_empty() {
+        egui::CollapsingHeader::new(dir.name.as_str()).show(ui, |ui| {
+            dir.children.iter().for_each(|subdir| {
+                render_dir(subdir, ui);
+            })
+        });
+    } else {
+        ui.label(dir.name.as_str());
     }
 }
 
 pub fn render_manifest(manifest: &Manifest, ui: &mut Ui) {
-    egui::CollapsingHeader::new("Base Files").show(ui, |ui| {});
+    let mut content_root = dir("Base Files");
+    build_tree(
+        &mut content_root,
+        &manifest.content_files.iter().cloned().collect(),
+        0,
+    );
+    dbg!("{:?}", &content_root);
+    render_dir(&content_root, ui);
+    let mut aoc_root = dir("DLC Files");
+    build_tree(
+        &mut aoc_root,
+        &manifest.aoc_files.iter().cloned().collect(),
+        0,
+    );
+    render_dir(&aoc_root, ui);
 }
