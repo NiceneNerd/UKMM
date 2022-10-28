22dbbb0797173a740f331a6ff3f719f10986d6de
Caleb Smith <c.smith@tuta.io>
2022-10-28 10:47:48 -0500

    Use hashes for ParamData derive

--- uk-content-derive/src/aamp.rs
+++ uk-content-derive/src/aamp.rs
@@ -3,6 +3,25 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::*;
 
+const fn hash_name(name: &str) -> u32 {
+    let mut crc = 0xFFFFFFFF;
+    let mut i = 0;
+    while i < name.len() {
+        crc ^= name.as_bytes()[i] as u32;
+        let mut j = 0;
+        while j < 8 {
+            if crc & 1 == 1 {
+                crc = (crc >> 1) ^ 0xEDB88320;
+            } else {
+                crc >>= 1;
+            }
+            j += 1;
+        }
+        i += 1;
+    }
+    !crc
+}
+
 fn field_from_param(
     ty: &Type,
     field_var_name: &Ident,
@@ -12,6 +31,7 @@ fn field_from_param(
     let Type::Path(ref ty_path) = ty else {
         panic!("invalid field type")
     };
+    let hash = hash_name(&field_src_name);
     if ty_path
         .path
         .segments
@@ -19,16 +39,18 @@ fn field_from_param(
         .any(|s| s.ident.to_string().as_str() == "Option")
     {
         quote! {
-            let #field_var_name: #ty = hash
-                .get(#field_src_name)
+            /// #hash = #field_src_name
+            let #field_var_name: #ty = obj
+                .get(#hash)
                 .map(|val| val.clone().try_into())
                 .transpose()
                 .map_err(|param| crate::UKError::InvalidParameter(#field_src_name.into(), param))?;
         }
     } else {
         quote! {
-            let #field_var_name: #ty = hash
-                .get(#field_src_name)
+            /// #hash = #field_src_name
+            let #field_var_name: #ty = obj
+                .get(#hash)
                 .cloned()
                 .ok_or(UKError::MissingAampKey(#err_msg))?
                 .try_into()
@@ -66,6 +88,7 @@ fn field_to_param(ty: &Type, field_var_name: &Ident, field_src_name: String) -> 
     let Type::Path(ref ty_path) = ty else {
         panic!("invalid field type")
     };
+    let hash = hash_name(&field_src_name);
     if ty_path
         .path
         .segments
@@ -74,12 +97,14 @@ fn field_to_param(ty: &Type, field_var_name: &Ident, field_src_name: String) -> 
     {
         quote! {
             if let Some(#field_var_name) = val.#field_var_name {
-                obj.insert(#field_src_name, #field_var_name.into());
+                /// #hash = #field_src_name
+                obj.insert(#hash, #field_var_name.into());
             }
         }
     } else {
         quote! {
-            obj.insert(#field_src_name, val.#field_var_name.into());
+            /// #hash = #field_src_name
+            obj.insert(#hash, val.#field_var_name.into());
         }
     }
 }
