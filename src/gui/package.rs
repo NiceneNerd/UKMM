98c63a7ff80804c0844c2a6ff1b4c11a2ddec539
Caleb Smith <c.smith@tuta.io>
2022-11-10 16:58:43 -0600

    Blah blah

--- src/gui/package.rs
+++ src/gui/package.rs
@@ -9,7 +9,7 @@ use uk_ui::{
     editor::EditableValue,
     egui::{self, Align2, Context, Id, Layout, Response, TextStyle, Ui},
     ext::UiExt,
-    icons::IconButtonExt,
+    icons::{Icon, IconButtonExt},
 };
 
 use super::{App, Message};
@@ -66,181 +66,158 @@ fn render_field(name: &str, ui: &mut Ui, add_contents: impl FnOnce(&mut Ui) -> R
 
 impl App {
     fn render_package_deps(&self, ctx: &Context, builder: &mut ModPackerBuilder) {
-        if self.show_package_deps {
-            egui::Window::new("Select Dependencies")
-                .anchor(Align2::CENTER_CENTER, [0., 0.])
-                .show(ctx, |ui| {
-                    egui::ScrollArea::new([true, false])
-                        .id_source("modal-pkg-deps")
-                        .show_rows(
-                            ui,
-                            ui.text_style_height(&TextStyle::Body),
-                            self.mods.len(),
-                            |ui, range| {
-                                for mod_ in self
-                                    .mods
-                                    .iter()
-                                    .skip(range.start)
-                                    .take(range.end - range.start)
-                                {
-                                    let mut in_deps =
-                                        builder.meta.masters.contains_key(&mod_.hash());
-                                    if ui
-                                        .checkbox(
-                                            &mut in_deps,
-                                            format!(
-                                                " {} (v{})",
-                                                mod_.meta.name.as_str(),
-                                                mod_.meta.version
-                                            ),
-                                        )
-                                        .changed()
-                                    {
-                                        if in_deps {
-                                            builder.meta.masters.insert(
-                                                mod_.hash(),
-                                                (mod_.meta.name.clone(), mod_.meta.version),
-                                            );
-                                        } else {
-                                            builder.meta.masters.shift_remove(&mod_.hash());
-                                        }
+        if !self.show_package_deps {
+            return;
+        }
+        egui::Window::new("Select Dependencies")
+            .anchor(Align2::CENTER_CENTER, [0., 0.])
+            .show(ctx, |ui| {
+                egui::ScrollArea::new([true, false])
+                    .id_source("modal-pkg-deps")
+                    .show_rows(
+                        ui,
+                        ui.text_style_height(&TextStyle::Body),
+                        self.mods.len(),
+                        |ui, range| {
+                            for mod_ in self
+                                .mods
+                                .iter()
+                                .skip(range.start)
+                                .take(range.end - range.start)
+                            {
+                                let mut in_deps = builder.meta.masters.contains_key(&mod_.hash());
+                                let friendly = format!(
+                                    " {} (v{})",
+                                    mod_.meta.name.as_str(),
+                                    mod_.meta.version
+                                );
+                                if ui.checkbox(&mut in_deps, friendly).changed() {
+                                    if in_deps {
+                                        builder.meta.masters.insert(
+                                            mod_.hash(),
+                                            (mod_.meta.name.clone(), mod_.meta.version),
+                                        );
+                                    } else {
+                                        builder.meta.masters.shift_remove(&mod_.hash());
                                     }
                                 }
-                            },
-                        );
+                            }
+                        },
+                    );
+                ui.allocate_ui_with_layout(
+                    [ui.available_width(), ui.spacing().interact_size.y].into(),
+                    Layout::right_to_left(Align::Center),
+                    |ui| {
+                        if ui.button("OK").clicked() {
+                            self.do_update(Message::ClosePackagingDependencies);
+                        }
+                        ui.shrink_width_to_current();
+                    },
+                );
+            });
+    }
+
+    fn render_package_opts(&self, ctx: &Context, builder: &mut ModPackerBuilder) {
+        if let Some(ref folders) = self.opt_folders {
+            return;
+        }
+
+        #[inline]
+        fn render_opt(i: usize, opt_group: &mut OptionGroup, id: Id, ui: &mut Ui) {
+            let group_name = if opt_group.name().is_empty() {
+                "New Option Group"
+            } else {
+                opt_group.name()
+            };
+            egui::CollapsingHeader::new(group_name)
+                .default_open(true)
+                .show(ui, |ui| {
+                    ui.label("Group Name");
+                    opt_group.name_mut().edit_ui_with_id(ui, id.with(i));
+                    ui.label("Group Description");
+                    ui.text_edit_multiline(&mut uk_ui::editor::SmartStringWrapper(
+                        opt_group.description_mut(),
+                    ));
+                    ui.label("Group Type");
+                    ui.horizontal(|ui| {
+                        if ui
+                            .radio(matches!(opt_group, OptionGroup::Exclusive(_)), "Exclusive")
+                            .clicked()
+                        {
+                            *opt_group = OptionGroup::Exclusive(ExclusiveOptionGroup {
+                                default: None,
+                                name: std::mem::take(opt_group.name_mut()),
+                                description: std::mem::take(opt_group.description_mut()),
+                                options: std::mem::take(opt_group.options_mut()),
+                            });
+                        }
+                        if ui
+                            .radio(matches!(opt_group, OptionGroup::Multiple(_)), "Multiple")
+                            .clicked()
+                        {
+                            *opt_group = OptionGroup::Multiple(MultipleOptionGroup {
+                                defaults: Default::default(),
+                                name: std::mem::take(opt_group.name_mut()),
+                                description: std::mem::take(opt_group.description_mut()),
+                                options: std::mem::take(opt_group.options_mut()),
+                            });
+                        }
+                    });
+                    if let OptionGroup::Exclusive(group) = opt_group {
+                        let id = Id::new(group.name.as_str()).with("default");
+                        let def_name = group
+                            .default
+                            .as_ref()
+                            .and_then(|opt| {
+                                group
+                                    .options
+                                    .iter()
+                                    .find_map(|o| o.path.eq(opt).then(|| o.name.as_str()))
+                            })
+                            .unwrap_or("None");
+                        egui::ComboBox::new(id, "Default Option")
+                            .selected_text(def_name)
+                            .show_ui(ui, |ui| {
+                                group.options.iter().for_each(|opt| {
+                                    let selected = group.default.as_ref() == Some(&opt.path);
+                                    if ui.selectable_label(selected, opt.name.as_str()).clicked() {
+                                        group.default = Some(opt.path.clone());
+                                    }
+                                });
+                            });
+                    }
+                });
+        }
+
+        egui::Window::new("Configure Mod Options")
+            .anchor(Align2::CENTER_CENTER, [0., 0.])
+            .show(ctx, |ui| {
+                egui::Frame::none().inner_margin(8.0).show(ui, |ui| {
+                    ui.spacing_mut().item_spacing.y = 8.0;
+                    ui.horizontal(|ui| {
+                        if ui.icon_text_button("Add Option Group", Icon::Add).clicked() {
+                            builder
+                                .meta
+                                .options
+                                .push(OptionGroup::Multiple(Default::default()));
+                        }
+                    });
+                    let id = Id::new("opt-groups-");
+                    for (i, opt_group) in builder.meta.options.iter_mut().enumerate() {
+                        render_opt(i, opt_group, id, ui);
+                    }
                     ui.allocate_ui_with_layout(
                         [ui.available_width(), ui.spacing().interact_size.y].into(),
                         Layout::right_to_left(Align::Center),
                         |ui| {
                             if ui.button("OK").clicked() {
-                                self.do_update(Message::ClosePackagingDependencies);
+                                self.do_update(Message::ClosePackagingOptions);
                             }
                             ui.shrink_width_to_current();
                         },
                     );
                 });
-        }
-    }
-
-    fn render_package_opts(&self, ctx: &Context, builder: &mut ModPackerBuilder) {
-        if let Some(ref folders) = self.opt_folders {
-            egui::Window::new("Configure Mod Options")
-                .anchor(Align2::CENTER_CENTER, [0., 0.])
-                .show(ctx, |ui| {
-                    egui::Frame::none().inner_margin(8.0).show(ui, |ui| {
-                        ui.spacing_mut().item_spacing.y = 8.0;
-                        ui.horizontal(|ui| {
-                            if ui
-                                .icon_text_button("Add Option Group", uk_ui::icons::Icon::Add)
-                                .clicked()
-                            {
-                                builder
-                                    .meta
-                                    .options
-                                    .push(OptionGroup::Multiple(Default::default()));
-                            }
-                        });
-                        let id = Id::new("opt-groups-");
-                        for (i, opt_group) in builder.meta.options.iter_mut().enumerate() {
-                            let group_name = if opt_group.name().is_empty() {
-                                "New Option Group"
-                            } else {
-                                opt_group.name()
-                            };
-                            egui::CollapsingHeader::new(group_name)
-                                .default_open(true)
-                                .show(ui, |ui| {
-                                    ui.label("Group Name");
-                                    opt_group.name_mut().edit_ui_with_id(ui, id.with(i));
-                                    ui.label("Group Description");
-                                    ui.text_edit_multiline(&mut uk_ui::editor::SmartStringWrapper(
-                                        opt_group.description_mut(),
-                                    ));
-                                    ui.label("Group Type");
-                                    ui.horizontal(|ui| {
-                                        if ui
-                                            .radio(
-                                                matches!(opt_group, OptionGroup::Exclusive(_)),
-                                                "Exclusive",
-                                            )
-                                            .clicked()
-                                        {
-                                            *opt_group =
-                                                OptionGroup::Exclusive(ExclusiveOptionGroup {
-                                                    default: None,
-                                                    name: std::mem::take(opt_group.name_mut()),
-                                                    description: std::mem::take(
-                                                        opt_group.description_mut(),
-                                                    ),
-                                                    options: std::mem::take(
-                                                        opt_group.options_mut(),
-                                                    ),
-                                                });
-                                        }
-                                        if ui
-                                            .radio(
-                                                matches!(opt_group, OptionGroup::Multiple(_)),
-                                                "Multiple",
-                                            )
-                                            .clicked()
-                                        {
-                                            *opt_group =
-                                                OptionGroup::Multiple(MultipleOptionGroup {
-                                                    defaults: Default::default(),
-                                                    name: std::mem::take(opt_group.name_mut()),
-                                                    description: std::mem::take(
-                                                        opt_group.description_mut(),
-                                                    ),
-                                                    options: std::mem::take(
-                                                        opt_group.options_mut(),
-                                                    ),
-                                                });
-                                        }
-                                    });
-                                    if let OptionGroup::Exclusive(group) = opt_group {
-                                        let id = Id::new(group.name.as_str()).with("default");
-                                        let def_name = group
-                                            .default
-                                            .as_ref()
-                                            .and_then(|opt| {
-                                                group.options.iter().find_map(|o| {
-                                                    o.path.eq(opt).then(|| o.name.as_str())
-                                                })
-                                            })
-                                            .unwrap_or("None");
-                                        egui::ComboBox::new(id, "Default Option")
-                                            .selected_text(def_name)
-                                            .show_ui(ui, |ui| {
-                                                group.options.iter().for_each(|opt| {
-                                                    let selected =
-                                                        group.default.as_ref() == Some(&opt.path);
-                                                    if ui
-                                                        .selectable_label(
-                                                            selected,
-                                                            opt.name.as_str(),
-                                                        )
-                                                        .clicked()
-                                                    {
-                                                        group.default = Some(opt.path.clone());
-                                                    }
-                                                });
-                                            });
-                                    }
-                                });
-                        }
-                        ui.allocate_ui_with_layout(
-                            [ui.available_width(), ui.spacing().interact_size.y].into(),
-                            Layout::right_to_left(Align::Center),
-                            |ui| {
-                                if ui.button("OK").clicked() {
-                                    self.do_update(Message::ClosePackagingOptions);
-                                }
-                                ui.shrink_width_to_current();
-                            },
-                        );
-                    });
-                });
-        }
+            });
     }
 
     pub fn render_packger(&self, ui: &mut Ui) {
@@ -258,10 +235,7 @@ impl App {
             self.render_package_deps(ui.ctx(), &mut builder);
             self.render_package_opts(ui.ctx(), &mut builder);
             ui.horizontal(|ui| {
-                if ui
-                    .icon_text_button("Manage Options", uk_ui::icons::Icon::Tune)
-                    .clicked()
-                {
+                if ui.icon_text_button("Manage Options", Icon::Tune).clicked() {
                     if let Ok(reader) = fs::read_dir(&builder.source) {
                         self.do_update(Message::ShowPackagingOptions(
                             reader
@@ -279,16 +253,13 @@ impl App {
                 let source_set = builder.source.exists();
                 ui.add_enabled_ui(source_set, |ui| {
                     if ui
-                        .icon_text_button("Set Dependencies", uk_ui::icons::Icon::List)
+                        .icon_text_button("Set Dependencies", Icon::List)
                         .clicked()
                     {
                         self.do_update(Message::ShowPackagingDependencies);
                     }
                 });
-                if ui
-                    .icon_text_button("Help", uk_ui::icons::Icon::Help)
-                    .clicked()
-                {
+                if ui.icon_text_button("Help", Icon::Help).clicked() {
                     open::that("https://nicenenerd.github.io/ukmm/packaging.html").unwrap_or(());
                 }
             });
