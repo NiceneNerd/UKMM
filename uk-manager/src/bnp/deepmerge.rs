df87fddc8de93ca8f08a05516c67bdc02422879b
Caleb Smith <c.smith@tuta.io>
2023-01-03 23:12:54 -0600

    More rambling braining for deepmerge BNP conversion

--- uk-manager/src/bnp/deepmerge.rs
+++ uk-manager/src/bnp/deepmerge.rs
@@ -18,13 +18,43 @@ impl BnpConverter<'_> {
         let deepmerge_path = self.path.join("logs/deepmerge.aamp");
         if deepmerge_path.exists() {
             let pio = ParameterIO::from_binary(fs::read(deepmerge_path)?)?;
-            let file_list = pio
+            let diff = pio
                 .object("FileTable")
                 .context("Deepmerge log missing file table")?
                 .0
                 .values()
                 .filter_map(|s| s.as_str().ok())
-                .collect::<Vec<_>>();
+                .try_fold(
+                    FxHashMap::default(),
+                    |mut acc, file| -> Result<FxHashMap<String, DiffEntry>> {
+                        let parts = file.split("//").collect::<Vec<_>>();
+                        if parts.len() < 2 {
+                            return Ok(acc);
+                        }
+                        let root_path = parts[0];
+                        let root = acc
+                            .entry(root_path.to_string())
+                            .or_insert_with(|| DiffEntry::Sarc(Default::default()));
+                        let parent = if parts.len() == 3 {
+                            if let DiffEntry::Sarc(map) = acc
+                                .entry(root_path.to_string())
+                                .or_insert_with(|| DiffEntry::Sarc(Default::default()))
+                            {
+                                map
+                            } else {
+                                anyhow::bail!("Nonsense")
+                            }
+                        } else {
+                            &mut acc
+                        };
+                        let plist = pio
+                            .list(file)
+                            .cloned()
+                            .context("Missing entry in deepmerge log")?;
+                        parent.insert(parts[parts.len() - 1].to_string(), DiffEntry::Aamp(plist));
+                        Ok(acc)
+                    },
+                )?;
         }
         Ok(())
     }
