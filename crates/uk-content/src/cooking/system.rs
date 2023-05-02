4b4a1a7112d47e18c72739ab4c56fc69f6f94c2d
GingerAvalanche <chodness@gmail.com>
2023-05-02 14:57:00 -0700

    Misc fixes
    Parse negative Recipe values as unsigned
    Get correct node values for CEI

--- crates/uk-content/src/cooking/system.rs
+++ crates/uk-content/src/cooking/system.rs
@@ -201,26 +201,26 @@ impl TryFrom<&Byml> for Cei {
                 .as_float()
                 .map_err(|_| UKError::WrongBymlType("not a float".into(), "a float"))
                 .unwrap(),
-            ma:  hash
-                .get("BT")
-                .ok_or(UKError::MissingBymlKey("CEI missing MA"))?
+            ma: hash
+                .get("Ma")
+                .ok_or(UKError::MissingBymlKey("CEI missing Ma"))?
                 .as_i32()
                 .map_err(|_| UKError::WrongBymlType("not an integer".into(), "an integer"))
                 .unwrap(),
-            mi:  hash
-                .get("BT")
-                .ok_or(UKError::MissingBymlKey("CEI missing MI"))?
+            mi: hash
+                .get("Mi")
+                .ok_or(UKError::MissingBymlKey("CEI missing Mi"))?
                 .as_i32()
                 .map_err(|_| UKError::WrongBymlType("not an integer".into(), "an integer"))
                 .unwrap(),
             ssa: hash
-                .get("BT")
+                .get("SSA")
                 .ok_or(UKError::MissingBymlKey("CEI missing SSA"))?
                 .as_i32()
                 .map_err(|_| UKError::WrongBymlType("not an integer".into(), "an integer"))
                 .unwrap(),
-            t:   hash
-                .get("BT")
+            t: hash
+                .get("T")
                 .ok_or(UKError::MissingBymlKey("CEI missing T"))?
                 .as_int::<i32>()
                 .map_err(|_| UKError::WrongBymlType("not an integer".into(), "an integer"))
