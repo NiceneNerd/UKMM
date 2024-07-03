281ab5675c67978bc17677e854c22066c86c8b17
Caleb Smith <c.smith@tuta.io>
2024-07-02 21:14:15 -0500

    Restore missing Package button to Window menu

--- src/gui/menus.rs
+++ src/gui/menus.rs
@@ -72,6 +72,7 @@ impl App {
             Tabs::Install,
             Tabs::Deploy,
             Tabs::Mods,
+            Tabs::Package,
             Tabs::Settings,
             Tabs::Log,
         ] {
